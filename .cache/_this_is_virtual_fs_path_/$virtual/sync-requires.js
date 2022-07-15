a81d079f73a76c54c5a6d2e6229329cd091e7d55
Stephen Hathaway <stphnhthwy@gmail.com>
2022-07-15 09:44:06 -0600

    Initial commit

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,11 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/sh/Development/gatsby-tutorial/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/sh/Development/gatsby-tutorial/src/pages/404.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/sh/Development/gatsby-tutorial/src/pages/index.js"))
+}
+
---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Tutorial`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
